069a722534ee86285f9f3cd6cd5075bd8f13c455
lucasaecio <lucasaeciomm@gmail.com>
2021-02-26 22:47:51 -0300

    Implementação do header e footer + Implementação do css principal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,82 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> 
-  </div>
-  <router-view/>
+<div id="app">
+  <theHeader/>
+  <main id="main">
+    <router-view/>
+  </main>
+  <theFooter/>
+</div>
 </template>
 
+<script>
+import theHeader from '@/components/theHeader.vue'
+import theFooter from '@/components/theFooter.vue'
+
+export default {
+  components: {
+    theHeader,
+    theFooter
+  }
+}
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body, ul, li, h1, h2, p {
+  margin: 0px;
+  padding: 0px;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url('./assets/pattern.svg') repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: Arial, Helvetica, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
 </style>
