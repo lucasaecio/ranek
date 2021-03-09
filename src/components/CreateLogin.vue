153c125a20f15300b98415f60d9f2db14dd61c8e
lucasaecio <lucasaeciomm@gmail.com>
2021-03-08 22:02:28 -0300

    Criação e implementação do formulário de novo usuário

--- src/components/CreateLogin.vue
+++ src/components/CreateLogin.vue
@@ -1,13 +1,44 @@
 <template>
-  <h1>Create login</h1>
+<section>
+    <h2>Crie sua conta</h2>
+    <transition mode="out-in">
+        <button v-if="!criar" class="btn" @click="criar = true"> Criar Conta</button>
+        <user-form v-else>
+            <button class="btn btn-form">Criar Usuário</button>
+        </user-form>
+    </transition>
+</section>
 </template>
 
 <script>
+import UserForm from '@/components/UserForm.vue'
+
 export default {
-    name: 'CreateLogin'
+    name: 'CreateLogin',
+    components: { UserForm },
+    data () {
+        return {
+            criar: false
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+h2 {
+    text-align: center;
+    margin-top: 40px;
+    margin-bottom: 10px;
+}
 
+.btn {
+    width: 100%;
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.btn-form {
+    max-width: 100%;
+}
 </style>
