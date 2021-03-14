08b41b891c024f0809f940a6432ac237e90fe8c9
lucasaecio <lucasaeciomm@gmail.com>
2021-03-13 23:46:24 -0300

    Implementação da área de vendas do usuário

--- src/views/profile/UserPurchase.vue
+++ src/views/profile/UserPurchase.vue
@@ -1,15 +1,64 @@
 <template>
-  <p>Compras do usuario
-      
-  </p>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div class="proutos-wrapper" v-for="(compra, index) in compras" :key="index">
+        <product-item v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor"><span>Vendedor</span> {{compra.vendedor_id}}</p>
+        </product-item>
+
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
+import ProductItem from '@/components/ProductItem.vue'
+import {mapState} from 'vuex'
+import {api} from '@/services.js'
 
+export default {
+  components: { 
+    ProductItem
+  },
+  data() {
+    return {
+      compras: null
+    }
+  },
+  computed: {
+    ...mapState(['user', 'isLoggedIn'])
+  },
+  methods: {
+    getCompras() {
+      api.get(`/transacao?comprador_id=${this.user.id}`).then(response => {
+        this.compras = response.data;
+      })
+    }
+  },
+  watch: {
+    isLoggedIn() {
+      this.getUserProducts();
+    }
+  },
+  created () {
+    if(this.isLoggedIn) {
+      this.getCompras();
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
 
+.vendedor span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 25px;
+}
 </style>
--- src/views/profile/UserSale.vue
+++ src/views/profile/UserSale.vue
@@ -1,13 +1,85 @@
 <template>
-  <p>Venda do usuario</p>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div class="proutos-wrapper" v-for="(venda, index) in vendas" :key="index">
+        <product-item v-if="venda.produto" :produto="venda.produto">
+          <p class="vendedor"><span>Comprador</span> {{venda.comprador_id}}</p>
+        </product-item>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco_id">
+            <li v-for="(value, key) in venda.endereco_id" :key="key">
+              {{key}}: {{value}}
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductItem from '@/components/ProductItem.vue'
+import {mapState} from 'vuex'
+import {api} from '@/services.js'
+
 export default {
+  components: { 
+    ProductItem
+  },
+  data() {
+    return {
+      vendas: null
+    }
+  },
+  computed: {
+    ...mapState(['user', 'isLoggedIn'])
+  },
+  methods: {
+    getVendas() {
+      api.get(`/transacao?vendedor_id=${this.user.id}`).then(response => {
+        this.vendas = response.data;
+      })
+    }
+  },
+  watch: {
+    isLoggedIn() {
+      this.getUserProducts();
+    }
+  },
+  created () {
+    if(this.isLoggedIn) {
+      this.getVendas();
+    }
+  }
 
 }
 </script>
 
-<style>
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
 
+.vendedor span {
+  color: #e80;
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom:60px;
+}
+
+h2 {
+  margin-bottom: 25px;
+}
+
+h3 {
+  margin: 0px;
+  justify-self: end;
+}
 </style>
